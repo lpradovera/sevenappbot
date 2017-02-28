3c606b16bbd17c4391a20274185953f905fa43ca
Luca Pradovera <luca.pradovera@gmail.com>
2017-02-28 16:41:18 +0100

    Switch to FB gem master

--- Gemfile
+++ Gemfile
@@ -1,6 +1,6 @@
 source 'https://rubygems.org'
 ruby '2.4.0'
 
-gem 'facebook-messenger'
+gem 'facebook-messenger', github: 'hyperoslo/facebook-messenger'
 gem 'wit'
 gem 'thin'
--- app.rb
+++ app.rb
@@ -2,22 +2,6 @@ require 'facebook/messenger'
 
 include Facebook::Messenger
 
-class BotLogic
-  def self.send_message(recipient, message)
-    Bot.deliver({
-      recipient: recipient,
-      message: message
-    }, access_token: ENV['ACCESS_TOKEN'])
-  end
-
-  def self.text_only_message(recipient, text_message)
-    message = {
-      text: text_message
-    }
-    send_message(recipient, message)
-  end
-end
-
 Bot.on :message do |message|
-  BotLogic.text_only_message(message.sender, 'Hello, human!')
+  message.reply(text: 'Hello, human!')
 end
